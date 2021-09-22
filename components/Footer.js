62612e2b158877920a6901cb0e59b4d32855e2a1
Aditya Joshi <1adityajoshi@gmail.com>
2021-09-22 15:21:04 -0700

    Add build

---
+++ components/Footer.js
@@ -0,0 +1,40 @@
+function Footer() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14">
+      <div className="space-y-4 text-xs text-gray-800 p-2">
+        <h5 className="font-bold">About</h5>
+        <p>How Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800  p-2">
+        <h5 className="font-bold">About</h5>
+        <p>How Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800  p-2">
+        <h5 className="font-bold">About</h5>
+        <p>How Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800 p-2">
+        <h5 className="font-bold">About</h5>
+        <p>How Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
